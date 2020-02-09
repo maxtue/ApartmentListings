83848c7d1faa5d79f8c183e1032b00a21220058a
Maximilian Rutz <max@Maximilians-MacBook-Pro.local>
2020-02-09 21:55:46 +0100

    included black formatter

--- webscraper.py
+++ webscraper.py
@@ -5,17 +5,29 @@ from datetime import datetime
 import pandas as pd
 import json
 
-for seite in range(1,3000):
-    
+for seite in range(1, 3000):
+
     print("Loop " + str(seite) + " startet.")
 
     df = pd.DataFrame()
-    l=[]
+    l = []
 
     try:
-        
-        soup = bs.BeautifulSoup(urllib.request.urlopen("https://www.immobilienscout24.de/Suche/S-2/P-"+str(seite)+"/Haus-Kauf").read(),'lxml')
-        print("Aktuelle Seite: "+"https://www.immobilienscout24.de/Suche/S-2/P-"+str(seite)+"/Haus-Kauf")
+
+        soup = bs.BeautifulSoup(
+            urllib.request.urlopen(
+                "https://www.immobilienscout24.de/Suche/S-2/P-"
+                + str(seite)
+                + "/Haus-Kauf"
+            ).read(),
+            "lxml",
+        )
+        print(
+            "Aktuelle Seite: "
+            + "https://www.immobilienscout24.de/Suche/S-2/P-"
+            + str(seite)
+            + "/Haus-Kauf"
+        )
         for paragraph in soup.find_all("a"):
 
             if r"/expose/" in str(paragraph.get("href")):
@@ -27,9 +39,22 @@ for seite in range(1,3000):
 
             try:
 
-                soup = bs.BeautifulSoup(urllib.request.urlopen('https://www.immobilienscout24.de'+item).read(),'lxml')
+                soup = bs.BeautifulSoup(
+                    urllib.request.urlopen(
+                        "https://www.immobilienscout24.de" + item
+                    ).read(),
+                    "lxml",
+                )
 
-                data = pd.DataFrame(json.loads(str(soup.find_all("script")).split("keyValues = ")[1].split("}")[0]+str("}")),index=[str(datetime.now())])
+                data = pd.DataFrame(
+                    json.loads(
+                        str(soup.find_all("script"))
+                        .split("keyValues = ")[1]
+                        .split("}")[0]
+                        + str("}")
+                    ),
+                    index=[str(datetime.now())],
+                )
 
                 data["URL"] = str(item)
 
@@ -42,17 +67,25 @@ for seite in range(1,3000):
 
                 df = df.append(data)
 
-            except Exception as e: 
-                print(str(datetime.now())+": " + str(e))
+            except Exception as e:
+                print(str(datetime.now()) + ": " + str(e))
                 l = list(filter(lambda x: x != item, l))
                 print("ID " + str(item) + " entfernt.")
         print("Exportiert CSV")
-        df.to_csv(".../rohdaten/"+str(datetime.now())[:19].replace(":","").replace(".","")+".csv",sep=";",decimal=",",encoding = "utf-8",index_label="timestamp")     
+        df.to_csv(
+            ".../rohdaten/"
+            + str(datetime.now())[:19].replace(":", "").replace(".", "")
+            + ".csv",
+            sep=";",
+            decimal=",",
+            encoding="utf-8",
+            index_label="timestamp",
+        )
 
         print("Loop " + str(seite) + " endet.\n")
-        
-    except Exception as e: 
-        print(str(datetime.now())+": " + str(e))
+
+    except Exception as e:
+        print(str(datetime.now()) + ": " + str(e))
 
 print("FERTIG!")
 
