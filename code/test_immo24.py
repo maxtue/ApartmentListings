8cd719599c3aa15ad2eab87dfbb856512a82ab9a
Maximilian Rutz <max@Maximilians-MBP.fritz.box>
2020-03-23 16:19:09 +0100

    renamed .gitignore

--- code/test_immo24.py
+++ code/test_immo24.py
@@ -4,7 +4,7 @@ import immo24
 
 class TestNameIrrelevant(unittest.TestCase):
     def test_get_data(self):
-        self.assertEqual(immo24.Immo24scrape.get_data(), 15)
+        self.assertEqual(immo24.Immo24scrape.scrape_data(), 15)
 
 
 if __name__ == "__main__":
